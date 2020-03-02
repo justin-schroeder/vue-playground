ee8334f25d2e95c7b8552da15f273dce4095ed13
Myer Nore <pnore@hbs.edu>
2020-03-02 13:02:14 -0600

    vueconf rrc - before workshop repo task #2 before livecoding - err

--- vueconf_redic_reuse_comp/workshop-resources/src/components/ConfirmationModal.vue
+++ vueconf_redic_reuse_comp/workshop-resources/src/components/ConfirmationModal.vue
@@ -1,11 +1,50 @@
 <template functional>
-  <div>
-  </div>
+    <div>
+        <AppModal>
+            <h3 class="confirmation-message">{{ message }}</h3>
+            <div class="buttons">
+                <AppButton @click="onConfirm()">
+                    Confirm
+                </AppButton>
+                <AppButton type="secondary" @click="onCancel()">
+                    Cancel
+                </AppButton>
+            </div>
+        </AppModal>
+    </div>
 </template>
 
 <script>
-export default {
-}
+  import AppModal from './AppModal'
+
+  export default {
+    props: {
+      message: {
+        type: String,
+        default: 'This Is The Default Message In Confirmation Modal'
+      },
+    },
+    data() {
+      return {
+        isModalOpen: false
+      }
+    },
+    components: {
+      AppModal
+    },
+    methods: {
+      onConfirm: function() {
+        this.isModalOpen = false;
+        this.$emit('confirm')
+        console.log('caught onConfirm')
+      },
+      onCancel: function() {
+        this.isModalOpen = false;
+        this.$emit('close')
+        console.log('caught onCancel')
+      }
+    }
+  }
 </script>
 
 <style lang="css" scoped>
--- vueconf_redic_reuse_comp/workshop-resources/src/tasks/Task2.vue
+++ vueconf_redic_reuse_comp/workshop-resources/src/tasks/Task2.vue
@@ -1,95 +1,95 @@
 <template>
-  <div>
-    <h1 class="text-2xl font-bold mb-4">Task 2: Reusable Component Composition</h1>
-    <ol class="steps">
-      <li>
-        Create a ConfirmationModal.vue that uses AppModal to that accepts a question and contains two buttons: "Confirm" and "Cancel"
-      </li>
-      <li>
-        Make the ConfirmationModal component emit a 'confirm' event when clicked on 'Confirm' button. And a 'close' event when clicking on 'Cancel'.
-      </li>
-      <li>
-        Display the confirmation question before the button, similar to the example below.
-      </li>
-      <li>
-        Make sure the ConfirmationModal looks like the example below, this includes the H3 styling and buttons positioning.
-      </li>
-    </ol>
+    <div>
+        <h1 class="text-2xl font-bold mb-4">Task 2: Reusable Component Composition</h1>
+        <ol class="steps">
+            <li>
+                Create a ConfirmationModal.vue that uses AppModal to that accepts a question and contains two buttons: "Confirm" and "Cancel"
+            </li>
+            <li>
+                Make the ConfirmationModal component emit a 'confirm' event when clicked on 'Confirm' button. And a 'close' event when clicking on 'Cancel'.
+            </li>
+            <li>
+                Display the confirmation question before the button, similar to the example below.
+            </li>
+            <li>
+                Make sure the ConfirmationModal looks like the example below, this includes the H3 styling and buttons positioning.
+            </li>
+        </ol>
 
-    <!-- TASK BEGINS HERE -->
-    <AppButton
-      @click="isModalOpen = true"
-      type="danger"
-    >
-      Don’t push that button!
-    </AppButton>
-
-    <AppModal
-      v-if="isModalOpen"
-      @close="isModalOpen = false"
-    >
-      <h3 class="confirmation-message">
-        So you really did it?
-      </h3>
-      <div class="buttons">
-        <AppButton @click="confirm">
-          Confirm
+        <!-- TASK BEGINS HERE -->
+        <AppButton
+                @click="isModalOpen = true"
+                type="danger"
+        >
+            Don’t push that button!
         </AppButton>
-        <AppButton type="secondary" @click="isModalOpen = false">
-          Cancel
-        </AppButton>
-      </div>
-    </AppModal>
 
-    <!-- DESIRED USAGE. UNCOMMENT WHEN READY -->
-    <!-- <ConfirmationModal
-      v-if="isModalOpen"
-      message="Have you finished the task?"
-      @confirm="confirm"
-      @close="isModalOpen = false"
-    /> -->
-    <!-- TASK ENDS HERE -->
-  </div>
+        <AppModal
+                v-if="isModalOpen"
+                @close="isModalOpen = false"
+        >
+            <h3 class="confirmation-message">
+                So you really did it?
+            </h3>
+            <div class="buttons">
+                <AppButton @click="confirm">
+                    Confirm
+                </AppButton>
+                <AppButton type="secondary" @click="isModalOpen = false">
+                    Cancel
+                </AppButton>
+            </div>
+        </AppModal>
+
+        <!-- DESIRED USAGE. UNCOMMENT WHEN READY -->
+        <ConfirmationModal
+                v-if="isModalOpen"
+                message="Have you finished the task?"
+                @confirm="confirm"
+                @close="isModalOpen = false"
+        />
+        <!-- TASK ENDS HERE -->
+    </div>
 </template>
 
 <script>
-import AppModal from '@/components/AppModal'
-// import ConfirmationModal from '@/components/ConfirmationModal'
+  import AppModal from '@/components/AppModal'
+  import ConfirmationModal from '@/components/ConfirmationModal'
 
-export default {
-  components: {
-    AppModal,
-    // ConfirmationModal
-  },
-  data () {
-    return {
-      isModalOpen: false
-    }
-  },
-  methods: {
-    confirm () {
-      alert('Why would you do that?')
-      this.isModalOpen = false
+  export default {
+    components: {
+      AppModal,
+      ConfirmationModal
+    },
+    data() {
+      return {
+        isModalOpen: false
+      }
+    },
+    methods: {
+      confirm() {
+        alert('Why would you do that?')
+        this.isModalOpen = false
+      }
     }
   }
-}
 </script>
 
 <style lang="sass">
-.confirmation-message
-  text-align: left
-  margin: 0
+    .confirmation-message
+        text-align: left
+        margin: 0
 
-.buttons
-  margin-top: 1rem
-  text-align: left
+    .buttons
+        margin-top: 1rem
+        text-align: left
 
-  button
-    margin-right: 1rem
+        button
+            margin-right: 1rem
 
-.steps
-  max-width: 30rem
-  text-align: left
-  margin: 0 auto 4rem
-  line-height: 1.6
+    .steps
+        max-width: 30rem
+        text-align: left
+        margin: 0 auto 4rem
+        line-height: 1.6
 </style>
